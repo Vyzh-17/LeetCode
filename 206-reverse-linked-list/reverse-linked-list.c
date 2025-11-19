bd47df13a01def32b90bffe378e8699c7970ad25
Vaishnavi D <vyshk17@gmail.com>
2025-11-19 15:15:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (92.91%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.c
@@ -0,0 +1,13 @@
+struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode* prev = NULL;
+    struct ListNode* temp = head;
+    struct ListNode* next = NULL;
+
+    while (temp != NULL) {
+        next = temp->next;  
+        temp->next = prev;  
+        prev = temp;         
+        temp = next;         
+    }
+    return prev;  
+}

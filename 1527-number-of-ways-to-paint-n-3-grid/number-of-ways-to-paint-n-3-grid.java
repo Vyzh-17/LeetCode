991fea70c626578a76c6b478772d695444fed167
Vaishnavi D <vyshk17@gmail.com>
2026-01-03 21:45:04 +0530

    Time: 2 ms (99.49%) | Memory: 42.2 MB (41.54%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
@@ -1,19 +1,13 @@
 class Solution {
     public int numOfWays(int n) {
-
-        int MOD = 1000000007;
-
-        long x = 6; 
-        long y = 6; 
-
+        long MOD = 1_000_000_007L;
+        long halfA = 6, halfB = 6;
         for (int i = 1; i < n; i++) {
-            long nx = (2 * x + 2 * y) % MOD;
-            long ny = (2 * x + 3 * y) % MOD;
-
-            x = nx;
-            y = ny;
+            long halfA2 = halfA * 3 + halfB * 2;
+            long halfB2 = halfA2 - halfA;
+            halfA = halfA2 % MOD;
+            halfB = halfB2 % MOD;
         }
-
-        return (int)((x + y) % MOD);
+        return (int)((halfA + halfB) % MOD);
     }
 }

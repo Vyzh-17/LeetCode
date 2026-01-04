34e102de6fb51905f4890e9e56dde504e9e7e85d
Vaishnavi D <vyshk17@gmail.com>
2026-01-04 22:29:15 +0530

    Time: 9 ms (97.47%) | Memory: 46.3 MB (68.18%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -1,35 +1,34 @@
 class Solution {
     public int sumFourDivisors(int[] nums) {
         int totalSum = 0;
-        
-        for (int n : nums) {
+
+        for (int num : nums) {
             int count = 0;
-            int currentSum = 0;
-            
-            // Iterate up to the square root of n
-            for (int d = 1; d * d <= n; d++) {
-                if (n % d == 0) {
-                    // d is a divisor
+            int sum = 0;
+
+            for (int i = 1; i * i <= num; i++) {
+                if (num % i == 0) {
+                    int d1 = i;
+                    int d2 = num / i;
+
+                    // first divisor
                     count++;
-                    currentSum += d;
-                    
-                    // If d is not the square root, add its counterpart n/d
-                    if (d * d != n) {
+                    sum += d1;
+
+                    // second divisor (if different)
+                    if (d1 != d2) {
                         count++;
-                        currentSum += n / d;
+                        sum += d2;
                     }
                 }
-                
-                // Optimization: if we already have more than 4, stop
                 if (count > 4) break;
             }
-            
-            // If exactly four divisors were found, add their sum to total
+
             if (count == 4) {
-                totalSum += currentSum;
+                totalSum += sum;
             }
         }
-        
+
         return totalSum;
     }
 }
